ae0c9d68bf89d0d5559f1e1da4fec66415feeacc
Rosaldo <153322410+marisol161@users.noreply.github.com>
2025-01-22 01:13:01 -0600

    Desafios diarios

---
+++ app.js
@@ -0,0 +1,16 @@
+// let bienvenidaMensaje= '¡Bienvenida y bienvenido a nuestro sitio web!';
+// let nombre = prompt('Por favor, ingresa tu nombre:');
+// let edad = prompt('Por favor ingresa tu edad');
+// let numeroDeVentas = 50;
+// let = saldoDisponible = 1000;
+// let mensajeError = '¡Error!, completa todos los campos'
+
+// alert(bienvenidaMensaje);
+// alert(mensajeError)
+
+// if  ( edad > 18 || edad == 18 ){
+//     alert(nombre +' ¡Puedes obtener tu licencia de conducir!')
+// } else {
+//     alert('¡Upss! '+ nombre +' parece ser que aún tienes que esperar')
+
+// }
