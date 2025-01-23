23143f0966c238b9cb58e276e178a80588e6fc83
Rosaldo <contacto@rombhus.com>
2025-01-23 00:37:52 -0600

    Desafios del módulo 2 y 3

---
+++ desafio3.js
@@ -0,0 +1,37 @@
+
+/*                          EJERCICIO 1 - MODULO 3                    */
+
+// let numero = 1;
+// while ( numero != 11 ) {
+//     console.log(`Número: ${numero}`);
+//     numero++;
+// }
+
+/*                          EJERCICIO 2 - MODULO 3                    */
+
+// let contador = 10;
+
+// while ( contador != 0 ){
+
+//     console.log(`Número: ${contador}`)
+
+//     contador--;
+// }
+
+/*                          EJERCICIO 3 - MODULO 3                    */
+
+// let numeroIngresado = parseInt(prompt("Ingresa un número: "));
+
+// while ( numeroIngresado  >=  0 ) {
+//     console.log(`Número: ${numeroIngresado}`);
+//     numeroIngresado--;
+// }
+
+/*                          EJERCICIO 4 - MODULO 3                    */
+
+let numero = parseInt(prompt("Ingresa un número: "));
+let contador = 0;
+while (contador <= numero) {
+    console.log(`Número: ${contador}`);
+    contador++;
+}
