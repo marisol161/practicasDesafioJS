a3cdfeda539f9d97894f9c37027666b26c2cc4c2
Rosaldo <153322410+marisol161@users.noreply.github.com>
2025-01-23 01:27:13 -0600

    Desafíos del cuarto módulo

---
+++ desafio4.js
@@ -0,0 +1,86 @@
+/*                          EJERCICIO 1 - MODULO 3                    */
+
+// console.log("Bienvenido, está es la implementación de un mensaje en consola")
+
+
+/*                          EJERCICIO 2 - MODULO 3                    */
+
+// let nombre = "Rosaldo";
+// console.log(`¡Hola, ${nombre}! en la consola del navegador`)
+
+
+/*                          EJERCICIO 3 - MODULO 3                    */
+
+// let nombre = "Rosaldo";
+// alert(`¡Hola, ${nombre}!`)
+
+
+/*                          EJERCICIO 4 - MODULO 3                    */
+// let respuestaPregunta = prompt("¿Cúal es el lenguaje de programación que más te gusta?");
+// console.log(`Respuesta: ${respuestaPregunta}`);
+
+
+/*                          EJERCICIO 5 - MODULO 3                    */
+
+// let valor1 = 5;
+// let valor2 = 9;
+// let resultado = (valor1 + valor2);
+// console.log(`La suma de ${valor1} + ${valor2} = ${resultado}`)
+
+
+/*                          EJERCICIO 6 - MODULO 3                    */
+
+// let valor1 = 550;
+// let valor2 = 233;
+// let resultado = (valor1-valor2);
+// console.log(`La diferencia entre ${valor1} - ${valor2} = ${resultado}`)
+
+
+/*                          EJERCICIO 7 - MODULO 3                    */
+
+// let edad = prompt("Por favor, ingrese su edad: ");
+// console.log(edad >= 18 ? "Eres mayor de edad" : "Eres menor de edad")
+
+
+/*                          EJERCICIO 8 - MODULO 3                    */
+// let numero = prompt("Ingresa un número: ");
+
+// if ( numero > 0 ) {
+//  console.log("El número es positivo");
+// } else if ( numero < 0 ) {
+//     console.log("El número es negativo");
+// } else{
+//     console.log("El número es 0");
+// }
+
+
+/*                          EJERCICIO 9 - MODULO 3                    */
+
+// let numero = 1;
+
+// while ( numero <= 10 ) {
+//     console.log(`Número: ${numero}`);
+//     numero++;
+// }
+
+
+/*                          EJERCICIO 10 - MODULO 3                    */
+
+// let nota = 8;
+// console.log(nota >= 7 ? "Aprobado" : "Reprobado");
+
+
+/*                          EJERCICIO 11- MODULO 3                    */
+
+// console.log(Math.random());
+
+
+/*                          EJERCICIO 12 - MODULO 3                    */
+
+// console.log(Math.floor(Math.random()*10)+1);
+
+
+// 13. Utiliza Math.random para generar un número entero entre 1 y 1000 y muestra ese número en la consola.
+/*                          EJERCICIO 13 - MODULO 3                    */
+
+console.log(Math.floor(Math.random()*1000)+1)
